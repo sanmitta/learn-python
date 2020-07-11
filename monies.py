8fbe577f39dc25021dac2140cdc6d768ff4c914c
Sanjeev KM <sanmitta@gmail.com>
2020-07-11 13:15:26 -0700

    added problem statement below

--- monies.py
+++ monies.py
@@ -24,3 +24,19 @@ print(results_dict)
 
 ## print(uniquenameslist)
 my_file.close()
+
+
+# I had to write a little python snippet at work today, and that gave me an idea of a small problem for this group:
+
+# Take a look at this file:
+# https://github.com/skgbanga/Sandbox/blob/master/tutorial/data
+
+# Each line of this file represents some pocket money given to that person. e.g. 
+# "shalabh 97"
+
+# means that 97$ were given to Shalabh. 
+
+# Now your task is:
+#  - print a list of people in decreasing order of the number of times they have received the pocket money
+#  - you should also print how many times they have received pocket money
+#  - top 5 pocket monies they have received.
