37e1021a478d3aaff603e47a0e5cb2c0d890f674
Sanjeev KM <sanmitta@gmail.com>
2020-07-11 23:54:25 -0700

    commit

--- monies.py
+++ monies.py
@@ -1,5 +1,6 @@
 my_file = open('mydata.txt', 'r')
 file_lines = my_file.readlines()
+my_file.close()
 
 rawnamelist = []
 for line in file_lines:
@@ -13,18 +14,21 @@ results_dict = {key: [] for key in uniquenameslist} # initialize
 
 for x in range(len(uniquenameslist)): # search for the same key in the whole file
     for line in file_lines:
-        print("line.split()[0]: " , line.split()[0])
-        print("x:", x)
+        #print("line.split()[0]: " , line.split()[0])
+        #print("x:", x)
         if line.split()[0] == uniquenameslist[x]:
             results_dict[uniquenameslist[x]].append(line.split()[1])
-    results_dict[uniquenameslist[x]].append(len(results_dict[uniquenameslist[x]]))
-    # i.e. last item in the list (dictionary's values) is the count of monies received by the person
+    print(uniquenameslist[x], " - " , sorted(results_dict.get(uniquenameslist[x]), reverse=True))
 
-print(results_dict)
 
-## print(uniquenameslist)
-my_file.close()
 
+# results_dict[uniquenameslist[x]].append(len(results_dict[uniquenameslist[x]])) 
+# I don't need this as I can get length at any time
+# i.e. last item in the list (dictionary's values) is the count of monies received by the person
+
+# print(results_dict)
+
+# print(uniquenameslist)
 
 # I had to write a little python snippet at work today, and that gave me an idea of a small problem for this group:
 
@@ -37,6 +41,6 @@ my_file.close()
 # means that 97$ were given to Shalabh. 
 
 # Now your task is:
-#  - print a list of people in decreasing order of the number of times they have received the pocket money
-#  - you should also print how many times they have received pocket money
-#  - top 5 pocket monies they have received.
+# print a list of people in decreasing order of the number of times they have received the pocket money
+# you should also print how many times they have received pocket money
+# top 5 pocket monies they have received
